e3262f3ae1fa815b5bb50982047f0e529eb97de2
evan <evan.short@gmail.com>
2013-08-28 17:47:10 -0400

    updating install

--- install.js
+++ install.js
@@ -8,3 +8,9 @@ var installer='./etc/profile_update';
 fs.readFile(installer, {encoding: 'utf8'}, function(err, data){
 	fs.createWriteStream(profile, {'flags': 'a'}).write(data);;
 });
+
+console.log("\n#######################################\n#");
+console.log("# Installation Success, I presume\n#");
+console.log("# Now Run this command:");
+console.log("# \tsource ~/.bash_profile");
+console.log("#\n#######################################\n");
