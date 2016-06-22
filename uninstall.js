5cbc7c99151572209901db89e0c764f4bd6db040
Matthew Ess <mess@purdue.edu>
2016-06-22 09:25:13 -0400

    Allow for non bash profiles, rename/reformat files
    install.js now asks what your shell profile is called, defaulting
    to .bash_profile. The profile scripts now set an environment var
    that will be checked on install to prevent double adding to the
    shell profile. The name of the shell profile is saved in a file in
    ~/.gotomark so it can be retrieved for uninstall.

--- uninstall.js
+++ uninstall.js
@@ -2,10 +2,10 @@
 
 var fs = require('fs');
 var homedir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-var profile=homedir+"/.bash_profile";
-var profile_update='./etc/bash_profile';
-var installer='./etc/profile_update';
-var markdir = homedir+"/.gotomark"; 
+var profile_add='./etc/profile';
+var installer='./etc/profile_installer';
+var markdir = homedir+"/.gotomark";
+var profile_loc=markdir+"/profile_loc"
 
 /*
 fs.exists(markdir, function (exists) {
@@ -18,15 +18,21 @@ fs.exists(markdir, function (exists) {
 	}
 });
 */
+fs.readFile(profile_loc, 'utf8', function (err, data) {
+	var profile = data;
+	console.log(data);
+	fs.readFile(profile, 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
 
-fs.readFile(profile, 'utf8', function (err,data) {
-	if (err) {
-		return console.log(err);
-	}
-
-	var result = data.replace(/(^\s*$\s)+#* added by gotomark #*$\s(^.*$\s)*^#*\shttps:.*gotomark\s#*$\s#*\s/mg, '');
-	fs.writeFile(profile, result, 'utf8', function (err) {
-		if (err) return console.log(err);
+		var result = data.replace(/(^\s*$\s)+#* added by gotomark #*$\s(^.*$\s)*^#*\shttps:.*gotomark\s#*$\s#*\s/mg, '');
+		fs.writeFile(profile, result, 'utf8', function (err) {
+			if (err) return console.log(err);
+			console.log("\n#######################################\n#");
+			console.log("# Please run \"export GOTOMARK=\" to reinstall in this shell");
+			console.log("#\n#######################################\n");
+		});
 	});
 });
 // http://stackoverflow.com/questions/14177087/replace-a-string-in-a-file-with-nodejs
